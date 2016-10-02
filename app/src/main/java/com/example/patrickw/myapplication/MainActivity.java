751a8fa346c878cfcebf3083cb9b284fdf7075ac
pjrwlazlo <pjrwlazlo@gmail.com>
2016-10-01 19:18:16 -0500

    Added Output Box

--- app/src/main/java/com/example/patrickw/myapplication/MainActivity.java
+++ app/src/main/java/com/example/patrickw/myapplication/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.patrickw.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.net.Uri;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class MainActivity extends Activity {
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    Button get_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        get_button = (Button) findViewById(R.id.Get_Button);
+        Toast.makeText(this, "hello", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+
     }
 }
